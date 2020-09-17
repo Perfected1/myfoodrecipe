07618cf19e857e85d0e248791f80988f412a78d4
Chike Nnamadim <38839028+Perfected1@users.noreply.github.com>
2020-09-17 08:20:09 +0100

    Routing

---
+++ src/Components/Recipe.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { ReactComponent } from "*.svg";
+
+class Recipe extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            
+            <div>
+                kdmvm
+            </div>
+        )
+    }
+}
+
+export default Recipe;
--- src/Components/Recipes.js
+++ src/Components/Recipes.js
@@ -1,4 +1,6 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
 
 const Recipes = props => (
     <div className="container">
@@ -11,10 +13,14 @@ const Recipes = props => (
                             <img className="recipe__box-img" src={recipe.recipe.image} alt={recipe.recipe.label} />
                             <div className="recipe__text">
                                 <h5 className="recipes__title">
-                                    {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 24)}...` }</h5>
+                                    {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0, 24)}...`}</h5>
                                 <p className="recipes__subtitle">Owners: <span>{recipe.recipe.source}</span></p>
                             </div>
-                            <button className="recipe_buttons">View Recipe</button>
+                            <button className="recipe_buttons">
+                                <Link to={{ pathname: `/recipe/${recipe.recipes_id}` }}>
+                                    View Recipe
+                                </Link>
+                            </button>
                         </div>
                     </div>
                 );
