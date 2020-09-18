32d3f238cd039385d15c27b1aa2bedb7add97157
Chike Nnamadim <38839028+Perfected1@users.noreply.github.com>
2020-09-18 11:22:27 +0100

    Update Recipe.js

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const APP_KEY = "7afbf54cc585d5c1840f901fb4396016"
 const APP_ID = "a6fad490"
@@ -12,16 +13,33 @@ class Recipe extends React.Component {
     }
     componentDidMount = async () => {
         const title = this.props.location.state.recipe;
-        const req = await fetch(`https://api.edamam.com/search?q=${title}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+        const req = await fetch(`https://api.edamam.com/search?q=${title}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=15&calories=591-722&health=alcohol-free`);
         const res = await req.json();
-        console.log(res.hits[0]);
-    
+        this.setState({ activeRecipe: res.hits[0].recipe });
     }
     render() {
-        console.log(this.props);
+        // label
+        const recipe = this.state.activeRecipe;
+        console.log(recipe);
         return (
             <div>
-                kdmvm
+                <div className="container">
+                    <div className="active-recipe">
+                        <h5>{recipe.label}</h5>
+                        <h5>jljj</h5>
+                        <img className="active-recipe__img" src={recipe.image} alt={recipe.label} />
+                        <h3 className="active-recipe__title">{recipe.label}</h3>
+        <h5 className="active-recipe__publisher">Publisher: <span>{recipe.source}</span></h5>
+        <p className="active-recipe__website">
+           Wesite: <span><a href={recipe.url}>{recipe.url}</a></span>
+        </p>
+        <button className="active-recipe_button">
+                                <Link to={{ pathname: `/` }}>
+                                    Go To Main Page
+                                </Link>
+                            </button>
+                    </div>
+                </div>
             </div>
         )
     }
